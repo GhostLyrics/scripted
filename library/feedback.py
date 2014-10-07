073ae85f1acb2c0cda5f3fb212e99c81a778cfb5
Alexander Skiba <mail@ghostlyrics.net>
2014-10-07 17:09:07 +0200

    improve readability: empty lines

--- library/feedback.py
+++ library/feedback.py
@@ -16,9 +16,12 @@ SCRIPTNAME = "UNINITIALIZED"
 
 def register(name):
     """Register the name of the script which wants to provide feedback."""
+
     global SCRIPTNAME
     SCRIPTNAME = name
 
+
 def give(message):
     """Print a message to stdout, prefixed with the name of the script"""
+
     print SCRIPTNAME + ": " + message
