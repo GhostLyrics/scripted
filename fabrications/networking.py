f00f083bf707ded788c6498d3033c92e09d82a4c
Alexander Skiba <mail@ghostlyrics.net>
2014-11-11 16:57:53 +0100

    networking: improve readability, regex explanation

--- fabrications/networking.py
+++ fabrications/networking.py
@@ -26,10 +26,15 @@ def get_IPs():
     count = 0
     for line in lines:
         if re.match("eth", line) is not None:
+
             interface = lines[count].split()[0]
             mac_address = lines[count].split()[4]
-            ip_address = re.findall(r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+",
-                lines[count+1])[0]
+
+            # r: raw string
+            # [0-9]+: one or more digits
+            # \.: escape dot so it is not treated as regex syntax
+            regex = r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"
+            ip_address = re.findall(regex, lines[count+1])[0]
 
             print "IP: " + ip_address,
             print " on " + interface,
