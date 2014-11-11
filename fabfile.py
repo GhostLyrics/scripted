be6ffce01373fb2f7429dfe0878f159f57f00b2d
Alexander Skiba <mail@ghostlyrics.net>
2014-11-11 14:34:12 +0100

    start usage of fabric library for remote work
    - fabfile collects all commands from fabrications/
    - fabrications/ssh has get_fingerprint

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# disabling due to recommended usage of fabric API
+# pylint: disable=wildcard-import, unused-wildcard-import
+
+"""
+fabfile.py
+
+:copyright: (c) 2014 by Alexander Skiba <skiba@icg.tugraz.at>
+:licence: MIT
+:bugreports: skiba@icg.tugraz.at
+
+Collects all fabrications.
+
+"""
+
+from fabric.api import env
+env.use_ssh_config = True
+
+from fabrications.ssh import *
---
+++ fabrications/ssh.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+fabrications/ssh.py
+
+:copyright: (c) 2014 by Alexander Skiba <skiba@icg.tugraz.at>
+:licence: MIT
+:bugreports: skiba@icg.tugraz.at
+
+SSH related fabric commands
+
+"""
+
+from fabric.api import run, settings, hide, env
+import re
+
+def get_fingerprints():
+    """SSH: Retrieve a machine's fingerprints"""
+
+    ssh_key_locations = ["ssh_host_dsa_key",
+        "ssh_host_key",
+        "ssh_host_rsa_key",
+        "ssh_host_ecdsa_key",
+        ]
+
+    retrieved_keys = []
+
+    with settings(warn_only=True), hide("stdout", "warnings"):
+        for key in ssh_key_locations:
+            full_key = "/etc/ssh/" + key + ".pub"
+            found_key = run("ssh-keygen -l -f " + full_key)
+            if found_key is not None:
+                if re.match("[0-9]", found_key[0]) is not None:
+                    retrieved_keys.append(found_key)
+
+
+    print "\n"
+    print "Fingerprints for " + env.host
+    for key in retrieved_keys:
+        print key
+    print "\n"
