9bc168d4b0bd8448a8a007ce71259b0fc17df61d
Alexander Skiba <mail@ghostlyrics.net>
2014-11-24 23:40:40 +0100

    fab/cron: cleanup (linter)
    + consolidate print statements

--- fabrications/cron.py
+++ fabrications/cron.py
@@ -2,35 +2,29 @@
 # -*- coding: utf-8 -*-
 
 """
-fabrications/cron.py
+Cronjob fabric commands.
 
 :copyright: (c) 2014 by Alexander Skiba <skiba@icg.tugraz.at>
 :licence: MIT
 :bugreports: skiba@icg.tugraz.at
 
-Cronjob fabric commands
-
 """
 
 from fabric.api import run, settings, hide, env, task
 
+
+# tasks #######################################################################
+
+
 @task
 def get_cronjobs():
-    """CRON: Retrieve lists of custom and scheduled cronjobs"""
-
+    """CRON: Retrieve lists of custom and scheduled cronjobs."""
     with settings(warn_only=True), hide("stdout", "warnings"):
         crontab = run("crontab -l")
         etc_cronjobs = run("ls /etc/cron.*")
         cronfile = run("cat /etc/crontab | grep ^[0-9]")
 
-    print "\n"
-    print "Cronjobs for " + env.host
-
-    print "Content of crontab: "
-    print crontab + "\n"
-
-    print "Scheduled cronjobs: "
-    print etc_cronjobs + "\n"
-
-    print "Jobs in custom cronfile: "
-    print cronfile + "\n"
+    print "\nCronjobs for {}".format(env.host)
+    print "Content of crontab: {}\n".format(crontab)
+    print "Scheduled cronjobs: {}\n".format(etc_cronjobs)
+    print "Jobs in custom cronfile: {}\n".format(cronfile)
