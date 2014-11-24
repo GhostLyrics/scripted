24d48275cd4886b08267e3b9b050ea4b7ba5d6f7
Alexander Skiba <mail@ghostlyrics.net>
2014-11-24 23:24:46 +0100

    fab/dhcp: cleanup (linter)

--- fabrications/dhcp.py
+++ fabrications/dhcp.py
@@ -2,16 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-fabrications/dhcp.py
+DHCP Server fabric commands.
 
 :copyright: (c) 2014 by Alexander Skiba <skiba@icg.tugraz.at>
 :licence: MIT
 :bugreports: skiba@icg.tugraz.at
 
-DHCP Server fabric commands
-
 """
-
 
 from fabric.api import task, run, hosts
 from os.path import splitext, basename
@@ -19,13 +16,16 @@ import fabrications.configuration as config
 
 # module configuration ########################################################
 
+
 MODULE = splitext(basename(__file__))[0]
 CONFIGURATION = config.get_configuration(MODULE)
 
+
 # tasks #######################################################################
+
 
 @task
 @hosts(CONFIGURATION["hostname"])
 def restart_dhcp():
-    """DHCP: Restart the DHCP service"""
+    """DHCP: Restart the DHCP service."""
     run("service isc-dhcp-server restart")
