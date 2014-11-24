34edffb284223a1150a99479b3a3959f1fcf0dd9
Alexander Skiba <mail@ghostlyrics.net>
2014-11-24 23:36:18 +0100

    fab/cyrus: cleanup (linter)
    + use format() where applicable

--- fabrications/cyrus.py
+++ fabrications/cyrus.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-fabrications/cyrus.py
+Cyrus Mailserver fabric commands.
 
 :copyright: (c) 2014 by Alexander Skiba <skiba@icg.tugraz.at>
 :licence: MIT
 :bugreports: skiba@icg.tugraz.at
-
-Cyrus Mailserver fabric commands
 
 """
 
@@ -19,34 +17,41 @@ from os.path import splitext, basename
 
 # module configuration ########################################################
 
+
 MODULE = splitext(basename(__file__))[0]
 CONFIGURATION = config.get_configuration(MODULE)
 
+
 # internal functions ##########################################################
 
-def login(server, username, password):
-    """Authenticate to the IMAP server"""
 
+def login(server, username, password):
+    """Authenticate to the IMAP server."""
     connection = imaplib.IMAP4_SSL(server)
     connection.login(username, password)
     return connection
 
+
 # tasks #######################################################################
+
 
 @task
 def create_mailbox(mailbox_user):
     """CYRUS: Create a new mailbox for the specified user."""
-
     connection = login(CONFIGURATION["hostname"],
-        CONFIGURATION["username"],
-        CONFIGURATION["password"])
+                       CONFIGURATION["username"],
+                       CONFIGURATION["password"])
     try:
         connection.create("user." + mailbox_user)
+
         # this crazy syntax is required otherwise the module just gives errors
-        connection.setquota("user." + mailbox_user,
-            "(storage %s)" % CONFIGURATION["default_quota"])
+        connection.setquota(
+            "user.{}".format(mailbox_user),
+            "(storage {})".format(CONFIGURATION["default_quota"])
+
     finally:
         connection.logout()
+
 
 @task
 @hosts(CONFIGURATION["hostname"])
